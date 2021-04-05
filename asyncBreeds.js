2387e167a4b25ddcbcf3fce9f6ef88eed55b8c71
Luke Penner <lukepenner@hotmail.ca>
2021-04-05 15:49:57 -0700

    async testing complete

---
+++ asyncBreeds.js
@@ -0,0 +1,14 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
+    if (!error) callback(data);
+    else callback(undefined);
+  });
+  // ISSUE: Attempting to return data out here will also not work.
+  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
+};
+
+module.exports = breedDetailsFromFile;
